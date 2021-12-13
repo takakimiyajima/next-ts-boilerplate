2121ead11153557bd97e2561e227f880e854642c
Miyajima Takaki <takaki.mz@gmail.com>
2021-12-13 12:10:26 -0800

    add: initial setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
+/**
+ * The process required for all pages can be implemented by writing it in here.
+ * 全ページに必要な処理はここに書くことで実装できます
+ */
+
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { UserAgentContextProvider } from '@/contexts/userAgent/userAgentProvider'
 import { THEME } from '@/constants/style'
+import '@/styles/destyle.css'
+import '@/styles/globals.css'
 
 type Props = {
   uaString: string
@@ -9,11 +16,13 @@ type Props = {
 
 function App({ Component, pageProps, uaString }: Props) {
   return (
-    <ThemeProvider theme={THEME}>
-      <UserAgentContextProvider uaString={uaString}>
-        <Component {...pageProps} />
-      </UserAgentContextProvider>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={THEME}>
+        <UserAgentContextProvider uaString={uaString}>
+          <Component {...pageProps} />
+        </UserAgentContextProvider>
+      </ThemeProvider>
+    </>
   )
 }
 
